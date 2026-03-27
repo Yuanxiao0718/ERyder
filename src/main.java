2c68b04e59586ea3491e41664347103389550084
Yuanxiao0718 <19556279310@163.com>
2026-03-27 13:50:22 +0800

    exercise8

--- src/main.java
+++ src/main.java
@@ -3,6 +3,13 @@ import java.util.Random;
 
 public class main {
     public static void Main(String[] args) {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.registration();
+        System.out.println(userRegistration);
+
+        AdminPanel adminPanel = new AdminPanel();
+        adminPanel.userManagementOptions();
+
         String sent1 = "I was very satisfied with the service.";
         String sent2 = "The e-Bike is quite comfortable to ride.";
         String sent3 = "The battery life of the e-Bike is impressive.";
@@ -20,23 +27,6 @@ public class main {
 
         System.out.println("************************");
 
-//        String regex = "^[0-9]{6}$";
-//        String inputID = "";
-//        while (true) {
-//            System.out.println("Enter the bike ID of the second bike:");
-//            Scanner sc2 = new Scanner(System.in);
-//            String bikeID2 = sc2.next();
-//
-//
-//            if (Pattern.matches(regex, bikeID2)) {
-//                inputID = bikeID2;
-//                break;
-//            } else {
-//                System.out.println("Error: BikeID must be a 6-digit number only! This assignment is invalid, default ID will be used：000000");
-//                // 非法值赋默认6位ID
-//            }
-//
-//        }
         Random random = new Random();
         int randomBatteryLevel = random.nextInt(101);
         double KmDriven = randomBatteryLevel * 0.9;
